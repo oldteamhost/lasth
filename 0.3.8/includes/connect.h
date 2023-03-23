25e114ade7d71bf3ebb2d3f3d31f42d54fc86613
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2023-03-23 23:05:05 +0300

    Delete 0.3.8 directory

--- 0.3.8/includes/connect.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class connect
-{
-public:
-	long responseCode(const char* link, bool _proxy, const char* proxy);
-	bool downloadPage(const char* link, const char* path);
-};
--- 0.3.8/includes/generate.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-class generate
-{
-public:
-	std::string generateString(std::size_t length, const std::string CHARACTERS);
-};
-
